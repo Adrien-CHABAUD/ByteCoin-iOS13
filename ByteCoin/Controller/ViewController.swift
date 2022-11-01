2da50e3efd0c1c2129d8b15a68b47d76c9951260
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2022-11-01 16:35:23 -0400

    Handling UIPicker functions.

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set the picker's datasource & delegate func
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+    
+    // Only one picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // There is as much rows than currencies
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    // Retrieve the currency labels to be displayed
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Get the selected currency
+        let currencySelected = coinManager.currencyArray[row]
+        // Pass the currency to the getCoinPrice function
+        coinManager.getCoinPrice(for: currencySelected)
     }
 
 
